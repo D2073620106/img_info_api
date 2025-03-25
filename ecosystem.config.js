3301efaad1388176d000667fb45359704292749d
DMBro <2073620106@qq.com>
2025-03-25 16:09:09 +0800

    pm2 配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 module.exports = {
   apps: [
     {
@@ -7,20 +8,27 @@ module.exports = {
       autorestart: true, // 自动重启
       watch: false, // 禁用文件监视
       max_memory_restart: '1G', // 内存超过1G自动重启
+      node_args: '-r dotenv/config',
+      env_file: path.resolve(__dirname, '.env'),
       env: {
         NODE_ENV: 'production',
-        PORT: 3000, // 你的应用端口
-        // 其他环境变量...
+        DOTENV_CONFIG_PATH: path.resolve(__dirname, '.env')
       },
-      env_production: {
-        NODE_ENV: 'production',
-        PORT: 3000,
-        // 生产环境特定变量...
-      },
+      // env: {
+      //   NODE_ENV: 'production',
+      //   PORT: 3000, // 你的应用端口
+      //   // 其他环境变量...
+      // },
+      // env_production: {
+      //   NODE_ENV: 'production',
+      //   PORT: 3000,
+      //   // 生产环境特定变量...
+      // },
       // 日志配置
       error_file: './logs/error.log',
       out_file: './logs/out.log',
       log_file: './logs/combined.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
       time: true, // 日志中显示时间
     },
   ],
