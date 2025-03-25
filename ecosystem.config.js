e253aeed7b40013ec85359dc0a1c9ba1442b8bed
DMBro <2073620106@qq.com>
2025-03-25 15:31:51 +0800

    pm2 配置文件

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps: [
+    {
+      name: 'img_info_parse', // 应用名称
+      script: 'dist/main.js', // 入口文件路径
+      instances: '2', // 根据CPU核心数启动最大实例数
+      autorestart: true, // 自动重启
+      watch: false, // 禁用文件监视
+      max_memory_restart: '1G', // 内存超过1G自动重启
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000, // 你的应用端口
+        // 其他环境变量...
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        // 生产环境特定变量...
+      },
+      // 日志配置
+      error_file: './logs/error.log',
+      out_file: './logs/out.log',
+      log_file: './logs/combined.log',
+      time: true, // 日志中显示时间
+    },
+  ],
+};
